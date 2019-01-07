66b0c5008ba862fbd66570dbd045d00d1ff8ad72
jsorensen <sorensej@us.ibm.com>
2019-01-07 16:03:06 -0600

    Wireframed an alternate method of managing server connection

--- node/server.js
+++ node/server.js
@@ -137,3 +137,23 @@ if (isDev) {
 
 //app.use('/api');
 //app.listen(443, () => console.log('app.listen ready'));
+
+
+// https://www.zeptobook.com/how-to-create-restful-crud-api-with-node-js-mongodb-and-express-js/
+/*
+// get dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+// parse requests
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+// default route
+app.get('/', (req, res) => {
+    res.json({"message": "Welcome to ZeptoBook Product app"});
+});
+// listen on port 3000
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
+*/
