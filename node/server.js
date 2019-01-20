34fa1f4e040fae46be541087ad3f24057fb7d9d5
jsorensen <sorensej@us.ibm.com>
2019-01-20 13:58:42 -0600

    Optional HTTPS method added

--- node/server.js
+++ node/server.js
@@ -157,3 +157,79 @@ app.listen(3000, () => {
     console.log("Server is listening on port 3000");
 });
 */
+
+
+
+
+
+
+
+
+
+// from https://medium.freecodecamp.org/the-definitive-node-js-handbook-6912378afc6e
+// Making HTTP requests with Node.js
+
+// How to perform HTTP requests with Node.js using GET, POST, PUT and DELETE.
+
+// I use the term HTTP, but HTTPS is what should be used everywhere, therefore these examples use HTTPS instead of HTTP.
+// Perform a GET Request
+
+// const https = require('https')
+// const options = {
+//   hostname: 'flaviocopes.com',
+//   port: 443,
+//   path: '/todos',
+//   method: 'GET'
+// }
+
+// const req = https.request(options, (res) => {
+//   console.log(`statusCode: ${res.statusCode}`)
+
+//   res.on('data', (d) => {
+//     process.stdout.write(d)
+//   })
+// })
+
+// req.on('error', (error) => {
+//   console.error(error)
+// })
+
+// req.end()
+
+// Perform a POST Request
+
+// const https = require('https')
+
+// const data = JSON.stringify({
+//   todo: 'Buy the milk'
+// })
+
+// const options = {
+//   hostname: 'flaviocopes.com',
+//   port: 443,
+//   path: '/todos',
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json',
+//     'Content-Length': data.length
+//   }
+// }
+
+// const req = https.request(options, (res) => {
+//   console.log(`statusCode: ${res.statusCode}`)
+
+//   res.on('data', (d) => {
+//     process.stdout.write(d)
+//   })
+// })
+
+// req.on('error', (error) => {
+//   console.error(error)
+// })
+
+// req.write(data)
+// req.end()
+
+// PUT and DELETE
+
+// PUT and DELETE requests use the same POST request format, and just change the options.method value.
