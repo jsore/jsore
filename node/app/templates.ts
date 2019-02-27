181808696045dd2f177dbea8c71d652b33929627
Justin Sorensen <jsore@users.noreply.github.com>
2019-02-27 20:33:09 +0000

    Update styles

--- node/app/templates.ts
+++ node/app/templates.ts
@@ -25,6 +25,15 @@ export const testBody = Handlebars.compile(`
         <div class="bio-background">
             <p class="bio-text">Abuse Analyst at IBM Cloud and Full Stack Developer</p>
         </div>
+        <div class="construction">
+            <p class="construction-text">
+                This site is still under construction.
+            </p>
+            <!-- <p class="github"> -->
+                <a href="https://github.com/jsore">Find me on GitHub for now!</a>
+            <!-- </p> -->
+        </div>
     </div>
 </div>
+
 `);
