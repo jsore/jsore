ca015ccd562de97968b6b7053183b8e1ca4f3150
Justin Sorensen <jus.a.sorensen@gmail.com>
2019-05-08 16:18:12 -0500

    Fix webpack logging, add better comments

--- node/webpack.config.js
+++ node/webpack.config.js
@@ -3,35 +3,36 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: './app/index.ts',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-        rules: [{
-            test: /\.ts$/,
-            loader: 'ts-loader',
-        },{
-            test: /\.css$/,
-            use: ['style-loader', 'css-loader'],
-        },{
-            test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-            loader: 'url-loader?limit=100000',
-        }]
-    },
-    plugins: [
-        /** HTML generator */
-        new HtmlWebpackPlugin({
-            title: 'jsore.com',
-        }),
-        /** for Bootstrap functionality */
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery',
-            'window.jQuery': 'jquery',
-            Popper: ['popper.js', 'default'],
-        }),
-    ],
+  mode: 'development',
+  entry: './app/index.ts',
+  stats: 'errors-only',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [{
+      test: /\.ts$/,
+      loader: 'ts-loader',
+    },{
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader'],
+    },{
+      test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+      loader: 'url-loader?limit=100000',
+    }]
+  },
+  plugins: [
+    /** HTML generator */
+    new HtmlWebpackPlugin({
+      title: 'jsore.com',
+    }),
+    /** for Bootstrap functionality */
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      Popper: ['popper.js', 'default'],
+    }),
+  ],
 };
